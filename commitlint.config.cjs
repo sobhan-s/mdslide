e19a8c5cad1000446a26870bc2c1ff83736ca1d2
sobhan-s <sobhans@mindfiresolutions.com>
2026-06-29 17:59:40 +0530

    chore(config): add scopes to commitlint

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'scope-enum': [2, 'always', ['parser', 'renderer', 'shared', 'core', 'cli', 'config', 'root']],
+    'scope-enum': [2, 'always', ['parser', 'renderer', 'shared', 'core', 'cli', 'config', 'root', 'mdslide-docs']],
     'scope-empty': [2, 'never'],
     'scope-case': [2, 'always', 'lower-case'],
   },
